9a5cc87118779d114ed04a2c93070efeb01e6b6d
yueyinqiu <lujiaqi2003@126.com>
2021-03-06 18:05:38 +0800

    good test

--- string-jointer/test/string-jointer.test.ts
+++ string-jointer/test/string-jointer.test.ts
@@ -1,9 +1,53 @@
 import { StringJointer } from "../src/string-jointer";
 
-var a = new StringJointer("sp","a","z");
-a.add("bc");
-a.add("de");
+function getEmptyJointer() {
+    var a = new StringJointer("nothing", "?", "!");
+    return a;
+}
+function getGreatJointer() {
+    var a = new StringJointer("great", "what", "jointer");
+    a.add("a");
+    a.add("string");
+    return a;
+}
+function getEmotionJointer() {
+    var a = new StringJointer("~awa~~~~~~~~~~~", "!!qaq!!!", "_+_+_+qwq");
+    a.add("|^-^|");
+    a.add("-_- -_-");
+    a.add("--");
+    return a;
+}
 
-test('hello world test', () => {
-    expect(a.toString()).toBe("abcspdez");
+test('pure add', () => {
+    expect(getGreatJointer().toString()).toBe("whatagreatstringjointer");
+    expect(getEmotionJointer().length).toBe(
+        "!!qaq!!!|^-^|~awa~~~~~~~~~~~-_- -_-~awa~~~~~~~~~~~--_+_+_+qwq".length);
+});
+
+test('merge', () => {
+    expect(getGreatJointer().merge(getEmotionJointer()).toString())
+        .toBe("whatagreatstringgreat|^-^|~awa~~~~~~~~~~~-_- -_-~awa~~~~~~~~~~~--jointer");
+});
+test('addMany', () => {
+    expect(getGreatJointer().addMany(["abc", "def"]).toString()).toBe("whatagreatstringgreatabcgreatdefjointer");
+    expect(getGreatJointer().addMany(["abc", "def"]).length).toBe("whatagreatstringgreatabcgreatdefjointer".length);
+    expect(getEmptyJointer().addMany(["abc", "def"]).toString()).toBe("?abcnothingdef!");
+
+    let arrayLike = {
+        0: 'abc',
+        1: 'def',
+        length: 2
+    }
+    expect(getGreatJointer().addMany(arrayLike).toString()).toBe("whatagreatstringgreatabcgreatdefjointer");
+    expect(getGreatJointer().addMany(arrayLike).length).toBe("whatagreatstringgreatabcgreatdefjointer".length);
+    expect(getEmptyJointer().addMany(arrayLike).toString()).toBe("?abcnothingdef!");
+
+    function* iterable()
+    {
+        yield "abc";
+        yield "def";
+    }
+    expect(getGreatJointer().addMany(iterable()).toString()).toBe("whatagreatstringgreatabcgreatdefjointer");
+    expect(getGreatJointer().addMany(iterable()).length).toBe("whatagreatstringgreatabcgreatdefjointer".length);
+    expect(getEmptyJointer().addMany(iterable()).toString()).toBe("?abcnothingdef!");
 });
